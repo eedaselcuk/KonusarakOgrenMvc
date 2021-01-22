041cca0f44c5c1204e537a02153ffd6b34f2cf54
Eda Selçuk <selcukelifeda@gmail.com>
2021-01-22 15:24:12 +0300

    Add files via upload

---
+++ Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KonusarakOgren.com.Models
+{
+    public class UserModel
+    {
+        public int _Id { get; set; }
+
+        public string _Username { get; set; }
+
+        public string _Password { get; set; }
+    }
+}
